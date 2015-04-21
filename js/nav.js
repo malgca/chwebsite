2b40fc02154429fed66502a7cbaeff53794a61c6
malgca <malgca.utility@gmail.com>
2015-04-21 20:51:45 +0200

    First commit to master branch. This will be counted as the master working commit.

---
+++ js/nav.js
@@ -0,0 +1,14 @@
+// CSS3 animated & responsive dropdown menu
+// Latest version: https://github.com/catalinred/Animenu
+
+(function(){
+		/* Mobile */
+		$('#menu-wrap').prepend('<div id="menu-trigger">Menu</div>');		
+		$("#menu-trigger").on("click", function(){
+			$("#menu").slideToggle();
+		});
+
+		// iPad
+		var isiPad = navigator.userAgent.match(/iPad/i) != null;
+		if (isiPad) $('#menu ul').addClass('no-transition');      
+})();         
