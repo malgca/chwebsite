7d82a3238c2f4de806354dd76ba858b714773832
malgca <malgca.utility@gmail.com>
2015-04-25 04:37:01 +0200

    Added some new scripts and images to make the footer more attractive. Completed cosmetic changes on the footer. Now the header then done.

---
+++ scripts/sitepages/chfooter.js
@@ -0,0 +1,75 @@
+window.onload = function() {
+
+	var setupPage = (function() {
+	// create the links that we'll require right up front
+	var imgLinks = {
+		imgFacebook: document.getElementById("link-facebook"),
+		imgTwitter: document.getElementById("link-twitter"),
+		imgLinkedIn: document.getElementById("link-linkedin"),
+		imgMail: document.getElementById("link-email")
+	}
+
+	// initialise the page to make sure we have the events required on startup
+	init = function() {
+		console.log(imgLinks.imgFacebook);
+		var imgEvents = (function(event) {
+			activateFacebook = function(event) {
+				document.getElementById("link-facebook").src = "images/social/facebook-active.png";
+			}
+
+			deactivateFacebook = function(event) {
+				document.getElementById("link-facebook").src = "images/social/facebook-inactive.png";
+			}
+
+			activateTwitter = function(event) {
+				document.getElementById("link-twitter").src = "images/social/twitter-active.png";
+			}
+
+			deactivateTwitter = function(event) {
+				document.getElementById("link-twitter").src = "images/social/twitter-inactive.png";
+			}
+
+			activateLinkedIn = function(event) {
+				document.getElementById("link-linkedin").src = "images/social/linkedin-active.png";
+			}
+
+			deactivateLinkedIn = function(event) {
+				document.getElementById("link-linkedin").src = "images/social/linkedin-inactive.png";
+			}
+
+			activateMail = function(event) {
+				document.getElementById("link-email").src = "images/social/email-active.png";
+			}
+
+			deactivateMail = function(event) {
+				document.getElementById("link-email").src = "images/social/email-inactive.png";
+			}
+
+			return {
+				activateFacebook: activateFacebook,
+				deactivateFacebook: deactivateFacebook,
+				activateTwitter: activateTwitter,
+				deactivateTwitter: deactivateTwitter,
+				activateLinkedIn: activateLinkedIn,
+				deactivateLinkedIn: deactivateLinkedIn,
+				activateMail: activateMail,
+				deactivateMail: deactivateMail
+			}
+		}());
+
+		imgLinks.imgFacebook.addEventListener("mouseenter", imgEvents.activateFacebook, false);
+		imgLinks.imgFacebook.addEventListener("mouseout", imgEvents.deactivateFacebook, false);
+		imgLinks.imgTwitter.addEventListener("mouseenter", imgEvents.activateTwitter, false);
+		imgLinks.imgTwitter.addEventListener("mouseout", imgEvents.deactivateTwitter, false);
+		imgLinks.imgLinkedIn.addEventListener("mouseenter", imgEvents.activateLinkedIn, false);
+		imgLinks.imgLinkedIn.addEventListener("mouseout", imgEvents.deactivateLinkedIn, false);
+		imgLinks.imgMail.addEventListener("mouseenter", imgEvents.activateMail, false);
+		imgLinks.imgMail.addEventListener("mouseout", imgEvents.deactivateMail, false);
+	}
+
+	// since the page has already been loaded, just call the method asap
+	init()
+
+	console.log("This actually happened");
+	}());
+}
